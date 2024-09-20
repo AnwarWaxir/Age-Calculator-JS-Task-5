31fc38bf02bf249e0cef124307dd557b62a5afed
AnwarWaxir <anwarhaali1@gmail.com>
2024-09-20 18:26:01 +0500

    change informations in day and month variables. creates 4 if conditions, first for to retrun and show alert if the date input is empty, second for to return and show alert if the DOB after than the current date, third for month and fourth for day. In last assign values to h1 and p tags.

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 document.getElementById('calculateBtn').addEventListener('click', function(){
-    var date_input = document.getElementById('date-input');
-    var final_date_input = date_input.value
-    var  year = document.getElementById('year-number')
-    var month = document.getElementById('month-number')
-    var day = document.getElementById('day-number')
+
+    var date_input = document.getElementById('date-input').value;
+    var outputYear = document.getElementById('year-number')
+    var outputMonth = document.getElementById('month-number')
+    var outputDay = document.getElementById('day-number')
+
+    if(!date_input) {
+        alert('Plz enter a valid date of birth.')
+        return;
+    }
 
     var now = new Date()
-    var dateOfBirth = new Date(final_date_input)
-    var totalTime = now - dateOfBirth;
+    var dateOfBirth = new Date(date_input)
+    
+    if(now < dateOfBirth) {
+        alert('Date of birth must be current date or earlier than current date.')
+        return;
+    }
 
-    var year = Math.floor(totalTime / (1000*60*60*24*365.25))
-    var month = Math.floor(totalTime / (1000*60*60*24*30.44))
-    var day = Math.floor(totalTime / (1000*60*60*24))
+    var totalTimeInMilliseconds = now - dateOfBirth;
+
+    // var year = now.getFullYear() - dateOfBirth.getFullYear(); // We can use also use this instead of bottom.
+    var year = Math.floor(totalTimeInMilliseconds / (1000*60*60*24*365.25));
+    // var month = Math.floor(totalTime / (1000*60*60*24*30.44)); // We can use also use this instead of bottom.
+    var month = now.getMonth() - dateOfBirth.getMonth();
+    var day = now.getDate() - dateOfBirth.getDate();
+    console.log(day)
+
+    if (month < 0 || (month === 0 && day < 0)) {
+        year--;
+        month += 12; // Add 12 months to make up for the previous year
+    }
+
+    if (day < 0) {
+        const prevMonth = new Date(now.getFullYear(), now.getMonth(), 0).getDate(); // Days in the previous month
+        day += prevMonth;
+        month--; // Here i decrement month means -1 form months
+    }
+
+    outputYear.textContent = year;
+    outputMonth.textContent = month;
+    outputDay.textContent = day;
 })
