5843afc26c568c90dfb4874bc1ba54e033dc667a
AnwarWaxir <anwarhaali1@gmail.com>
2024-09-29 13:16:31 +0500

    In this commit i have variable for date input and get value of this input

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('calculateBtn').addEventListener('click', function(){
+    var date_input = document.getElementById('date-input');
+    var final_date_input = date_input.value
+})
