d63fa707f0e7a29683cda6ddf28083905d8f3613
AnwarWaxir <anwarhaali1@gmail.com>
2024-09-29 13:22:35 +0500

    create 3 variable for years, months, day

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.getElementById('calculateBtn').addEventListener('click', function(){
     var date_input = document.getElementById('date-input');
     var final_date_input = date_input.value
+    var  year = document.getElementById('year-number')
+    var month = document.getElementById('month-number')
+    var day = document.getElementById('day-number')
 })
